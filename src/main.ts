59ac207faea88a23eb5810ed9a089887006094dd
mohitkmr700 <mohitkmr700@users.noreply.github.com>
2025-06-08 07:59:40 +0530

    port fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '127.0.0.1');
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
