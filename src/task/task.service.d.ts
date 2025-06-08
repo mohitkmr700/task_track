c41dc3d4803c7f6f28e80446c41f30d9ee4bfee3
mohitkmr700 <mohitkmr700@users.noreply.github.com>
2025-06-08 14:31:33 +0530

    fixed api delete

--- src/task/task.service.d.ts
+++ src/task/task.service.d.ts
@@ -2,5 +2,5 @@ export declare class TaskService {
     listTasks(email?: string): Promise<any>;
     createTask(data: any): Promise<any>;
     updateTask(data: any): Promise<any>;
-    deleteTask(email: string): Promise<any>;
+    deleteTask(id: string): Promise<any>;
 } 
