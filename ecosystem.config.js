32dd13ad6d0940109fe3a3b7c5560d8abecadfd6
mohitkmr700 <mohitkmr700@users.noreply.github.com>
2025-06-21 15:00:16 +0530

    Update ecosystem configuration for user-service: Add environment variables for Redis and PocketBase, and modify deployment command to use ecosystem.config.js for PM2 management.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'task-backend',
-      script: 'dist/main.js',
-      env: {
-        PORT: 3001,
-        NODE_ENV: 'production'  // optional, set your environment
-      }
+  apps: [{
+    name: 'user-service',
+    script: 'dist/main.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+      REDIS_PORT: process.env.REDIS_PORT || 6379,
+      REDIS_PASSWORD: process.env.REDIS_PASSWORD || '',
+      POCKETBASE_ADMIN_EMAIL: process.env.POCKETBASE_ADMIN_EMAIL || '',
+      POCKETBASE_ADMIN_PASSWORD: process.env.POCKETBASE_ADMIN_PASSWORD || '',
+      POCKETBASE_URL: process.env.POCKETBASE_URL || 'http://algoarena.co.in/pocketbase',
+      PORT: process.env.PORT || 3001
     }
-  ]
+  }]
 };
